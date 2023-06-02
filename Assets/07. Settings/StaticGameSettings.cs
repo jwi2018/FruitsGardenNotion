58a5dc683bfc012ea8c3d56126147a8feb2c7cb4
jwi <jwi2018@naver.com>
2023-06-02 19:08:32 +0900

    프로젝트 이동

---
+++ Assets/07. Settings/StaticGameSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public static class StaticGameSettings
+{
+    public static int TotalStage = 1800;
+
+    public static bool StageOpenDebug = true;
+
+    public static int TestPlusDay = 0;
+
+    public static int iLimitStageEventLevel = 24;
+
+    public static string ReviewAOSURL = "market://details?id=com.icecube.gardenmatch.android.free.puzzle";
+    public static string ReviewIOSURL = "itms-apps://itunes.apple.com/app/id1593152885";
+
+    public static string CsReportTitle = "Fruits Garden Match 3 Puzzle Cs Report";
+
+    public static float BounceValue = 0.4f;
+    
+    public static string AOSBannerKey = "aa72ae47c9624b48";
+    public static string AOSInterstitialKey = "411c543046af0100";
+    public static string AOSRewardKey = "f4eb6575c6da7913";
+
+    public static string IOSBannerKey = "da569e28b0a7f42d";
+    public static string IOSInterstitialKey = "2e9cce9d8af72e03";
+    public static string IOSRewardKey = "9457036268d7fadc";
+
+    public static string AOSNativeKey = "ca-app-pub-3636561524673964/6044569915";
+    public static string IOSNativeKey = "ca-app-pub-3636561524673964/7381702315";
+
+    public static bool IsUseDoubleClick = true;
+    public static bool IsAbleFish = true;
+}
